888216b053912b3112f9ecba2f1bb91336bc59aa
hesamd <hesamd@evenset.com>
2019-12-20 11:47:26 -0500

    added redact and last name plugins

--- masking_plugins/mask_lastname_randomize.py
+++ masking_plugins/mask_lastname_randomize.py
@@ -1,18 +1,18 @@
-""" First name randomized mask """
+""" Last name randomized mask """
 
 from random import sample
 from masking_plugins.Mask_abstract import Mask_abstract
-from Dictionaries.populate import first_names
+from Dictionaries.populate import last_names
 
 
-class MaskFirstNameRandomized(Mask_abstract):
-    """This class replace the first name with randomized name from the dictionary"""
+class MaskLastNameRandomized(Mask_abstract):
+    """This class replace the last name with randomized name from the dictionary"""
     def __init__(self):
         self.replacements = {}
 
     def mask(self, text_to_reduct) -> str:
         """ Replace the name with a random name from dictionary"""
         if text_to_reduct not in self.replacements.keys():
-            replacement = sample(first_names(), 1)
+            replacement = sample(last_names(), 1)
             self.replacements[text_to_reduct]= replacement[0].title()
         return self.replacements[text_to_reduct]
---
+++ masking_plugins/mask_redact.py
@@ -0,0 +1,17 @@
+""" First name randomized mask """
+
+from masking_plugins.Mask_abstract import Mask_abstract
+
+
+class MaskRedact(Mask_abstract):
+    """This class replace the first name with randomized name from the dictionary"""
+    def __init__(self):
+        self.replacements = {}
+
+    def mask(self, text_to_reduct, data_type) -> str:
+        """ Replace any give value with data type, e.g. replace 12-12-2019 with DATE
+            Or replace John with FIRST_NAME        
+        """
+        if text_to_reduct not in self.replacements.keys():
+            self.replacements[text_to_reduct] = data_type
+        return self.replacements[text_to_reduct]
